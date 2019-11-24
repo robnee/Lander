4a403c57d60161fce8a86e68dae0e2cd4577335c
Rob Nee <robnee@users.noreply.github.com>
2019-11-23 20:28:48 -0500

    clarify unit conversion from path to scene

--- lander.py
+++ lander.py
@@ -6,43 +6,78 @@ import math
 A = Action
 
 
+def make_path(points):
+    path = ui.Path()
+    path.line_cap_style = ui.LINE_CAP_ROUND
+    path.line_join_style = ui.LINE_JOIN_ROUND
+
+    move = True
+    for p in points:
+        if p:
+            x, y = p
+            if move:
+                path.move_to(x, -y)
+                move = False
+            else:
+                path.line_to(x, -y)
+        else:
+            move = True
+
+    return path
+
+
 class Particle:
     '''Physics for partical motion in 2D space'''
+
     def __init__(self):
         self.m = 0
         self.r = 0
+        self.x = 0
+        self.y = 0
         self.ar = 0.0
         self.vr = 0.0
         self.ax = 0.0
         self.ay = 0.0
         self.vx = 0.0
         self.vy = 0.0
-        self.x = 0
-        self.y = 0
-        
-    def update(self):
-        self.vr = self.vr * 0.98 + self.ar
+        self.fr = 0.99
+        self.gx = 0.0
+        self.gy = -0.025
+
+    def update(self, dt):
+        self.vr = self.vr * self.fr + self.ar
         self.r += self.vr
-        
-        gx, gy, gz = gravity()
-        gx, gy = 0, -0.025
-        self.vx = self.vx * 0.999 + self.ax + gx
-        self.vy = self.vy * 0.999 + self.ay + gy
+
+        self.vx = self.vx * 0.999 + self.ax + self.gx
+        self.vy = self.vy * 0.999 + self.ay + self.gy
         self.x += self.vx
         self.y += self.vy
         
-    def get_a(self):
+        # map to node
+        self.rotation = self.r
+
+    def null(self):
+        self.r = 0
+        self.vr = 0
+        self.vx = 0
+        self.vy = 0
+        
+    @property
+    def a(self):
         return abs(Point(self.ax, self.ay))
+        
+    @a.setter
+    def a(self, a):
+        self.ax = -math.sin(self.r) * a
+        self.ay = math.cos(self.r) * a
 
 
 class Part(ShapeNode, Particle):
-    def __init__(self, pt):
-        path = ui.Path()
+    def __init__(self, points):
+        self.points = points
+        path = make_path(points)
         path.line_width = 1.5
-        path.line_cap_style = ui.LINE_CAP_ROUND
-        path.line_join_style = ui.LINE_JOIN_ROUND
-        path.move_to(0, 0)
-        path.line_to(pt.x, pt.y)
+
         ShapeNode.__init__(self, path=path, fill_color='clear', stroke_color='#bbb')
         Particle.__init__(self)
 
@@ -92,7 +127,7 @@ class Ship(ShapeNode, Particle):
         self.ljet = ShapeNode(path=self.no_flame, fill_color=(0, 0, 1.0, 0.1), stroke_color=(0, 1.0, 1.0, 0.5), parent=self)
         self.rjet = ShapeNode(path=self.no_flame, fill_color=(0, 0, 1.0, 0.1), stroke_color=(0, 1.0, 1.0, 0.5), parent=self)
         
-        print('ship:', self.position)
+        self.maxalt = 0
         self.fuel = 5000
         self.mass = 1000
         self.thrust = 0
@@ -100,12 +135,6 @@ class Ship(ShapeNode, Particle):
         self.thrust_sound = Sound('thrust.mp3')
         self.add_child(self.thrust_sound)
             
-    def null(self):
-        self.r = math.pi/2
-        self.vr = 0
-        self.vx = 0
-        self.vy = 0
-  
     def update(self, dt):
         if self.thrust_ramp:
             self.set_thrust(max(self.thrust - self.thrust_ramp, 0))
@@ -115,25 +144,23 @@ class Ship(ShapeNode, Particle):
         if self.fuel < dfuel:
             self.set_thrust(self.fuel / (10 * dt))
             self.fuel = 0
-        else:  
+        else:
             self.fuel -= dfuel
-        
-        force = self.thrust * 50 # units
-        
-        a = force / (self.mass + self.fuel)
-        self.ax = math.cos(self.r) * a
-        self.ay = math.sin(self.r) * a
-        
-        Particle.update(self)
+
+        self.a = self.thrust * 50 / (self.mass + self.fuel)
+
+        self.maxalt = max(self.maxalt, self.y)
+
+        Particle.update(self, dt)
         
     def set_thrust(self, level):
         if self.fuel <= 0:
             level = 0
-            
+
         self.thrust = min(level, self.MAX_THRUST)
     
         self.flame.path = self.flame_path(self.thrust)
-        self.flame.position = ((-30 - self.flame.path.bounds.w) / 2, 0)
+        self.flame.position = (0, (-30 - self.flame.path.bounds.h) / 2)
         
         if level > 0.0:
             self.thrust_sound.play(self.thrust / self.MAX_THRUST)
@@ -144,26 +171,37 @@ class Ship(ShapeNode, Particle):
         self.thrust_sound.stop()
         sound.play_effect('explosion_large_distant.mp3')
         self.fuel = max(self.fuel - 50, 0)
-               
+
+        # create ship parts
+        prevx, prevy = self.points[0]
+        for x, y in self.points[1:]:
+            p = Part([(prevx, prevy), Point(x, y)])
+            p.x, p.y, p.r = self.x + (x + prevx) / 2, self.y + (y + prevy) / 2, self.r
+            
+            prevx, prevy = x, y
+            
+            p.vr = random.uniform(-0.2, 0.2)
+            p.vx = self.vx + random.uniform(-1, 1)
+            p.vy = random.uniform(0, 3)
+            p.run_action(A.sequence(A.fade_to(0.50, 5), A.remove()))
+
+            self.parent.add_child(p)
+
     def rotate(self, level):
         #force =
-        self.ar = max(min(-level / 1000, 0.01), -0.01)
+        self.ar = max(min(-level / 1500, 0.01), -0.01)
 
         tail = self.ar * 10000
         
-        path = ui.Path()
+        path = make_path([(0, 0), (tail, 0)])
         path.line_width = 2
-        path.line_cap_style = ui.LINE_CAP_ROUND
-        path.line_join_style = ui.LINE_JOIN_ROUND
-        path.move_to(0, 0)
-        path.line_to(0, tail)
                         
         if tail > 1:
-            self.ljet.position = (10, -5 - tail / 2)
+            self.ljet.position = (5 + tail / 2, 10)
             self.ljet.path = path
             self.rjet.path = self.no_flame
         elif tail < -1:
-            self.rjet.position = (10, 5 - tail/2)
+            self.rjet.position = (-5 + tail / 2, 10)
             self.rjet.path = path
             self.ljet.path = self.no_flame
         else:
@@ -171,44 +209,27 @@ class Ship(ShapeNode, Particle):
             self.ljet.path = self.no_flame
 
     def ship_path(self):
-        path = ui.Path()
+        self.points = [(-10, -15), (0, 15), (10,-15), (0, -10), (-10, -15)]
+        path = make_path(self.points)
         path.line_width = 1.5
-        path.line_cap_style = ui.LINE_CAP_ROUND
-        path.line_join_style = ui.LINE_JOIN_ROUND
-        #path.move_to(0, 0)
-        #path.line_to(30, 10)
-        #path.line_to(0, 20)
-        #path.line_to(5, 10)
-        #path.line_to(0, 0)
-        
-        self.points = [(-15, -10), (15, 0), (-15, 10), (-10, 0), (-15, -10)]
-        path.move_to(*self.points[0])
-        for p in self.points[1:]:
-            path.line_to(*p)
 
         return path
         
     def flame_path(self, level):
-        path = ui.Path()
         if level > 0:
             tail = level * 40
         
+            path = make_path([(0, 0), (5, -tail), (10, 0), (5, -3), (0, 0)])
             path.line_width = 1.25
-            path.line_cap_style = ui.LINE_CAP_ROUND
-            path.line_join_style = ui.LINE_JOIN_ROUND
-            path.move_to(0, 0)
-            path.line_to(-tail, 5)
-            path.line_to(0, 10)
-            path.line_to(-3, 5)
-            path.line_to(0, 0)
 
-        return path
+            return path
+        else:
+            return ui.Path()
         
         
 class Mountain:
     def __init__(self, size):
-        self.points = list()
-        self.points.append(Point(10, 10))
+        self.points = [Point(0, 0), Point(10, 10)]
         x, y = 10, 10
         while x < size.w:
             dx = random.uniform(50, 100)
@@ -218,14 +239,13 @@ class Mountain:
                 dy = 0
             x = min(x + dx, size.w)
             y = max(y + dy, 10)
-            p = Point(x, y)
-            self.points.append(p)
+            self.points.append(Point(x, y))
         
     def gen_path(self, scale):
-        path = ui.Path()
-        path.move_to(0, 0)
-        for p in self.points:
-            path.line_to(p.x/scale, -p.y/scale)
+        """ scale points and make path """
+
+        path = make_path([(x * scale, y * scale) for x, y in self.points])
+        path.line_width = 1.0
 
         return path
         
@@ -253,53 +273,55 @@ class Mountain:
         return False
 
     def is_above_ground(self, particle, dy):
-        #ground_level = self.get_y(p.x) + dy
-        #return p.y > ground_level
-        for px, py in particle.points:
-            if particle.y + py < self.get_y(particle.x + px):
-                return False
-                
-        return True
+        ground_level = self.get_y(particle.x) + dy
+        return particle.y > ground_level
 
 
 class MyScene(Scene):
     def setup(self):
-        self.label = LabelNode(text='', font=('Helvetica Neue', 20), parent=self)
+        print('setup')
+        self.paused = True
+        self.running = False
+        self.landings = 0
+
+        self.ship = Ship()
+        self.ship.z_position = 1
+        self.add_child(self.ship)
+
+        self.mtdata = Mountain(Size(5000, 300))
+        path = self.mtdata.gen_path(1/3)
+
+        self.mt = ShapeNode(path=path, fill_color=self.background_color, stroke_color='#bbb')
+        self.mt.scale = 3
+        self.mt.anchor_point = (0, 0)
+        self.z_position = 0
+        self.add_child(self.mt)
+
+        self.label = LabelNode(text='', font=('Menlo', 20), parent=self)
         self.label.position = (self.size.w / 2, self.size.h - 20)
         
         self.stats = LabelNode(text='stats', font=('Menlo', 16), parent=self)
         self.stats.position = (self.size.w / 2, self.size.h - 40)
         self.stats.z_position = 5
         
-        self.mtdata = Mountain(Size(5000, 300))
-        path = self.mtdata.gen_path(3)
-        path.line_width = 1.0
-        path.line_cap_style = ui.LINE_CAP_ROUND
-        path.line_join_style = ui.LINE_JOIN_ROUND
-
-        self.mt = ShapeNode(path=path, fill_color=self.background_color, stroke_color='#bbb', parent=self)
-        self.mt.scale = 3
-        self.mt.anchor_point = (0, 0)
-        self.z_position = 0
-        
-        self.ship = Ship()
-        self.ship.z_position = 1
-        self.add_child(self.ship)
-        
-        self.landings = 0
-        self.set_scale()
+        self.update_scale()
         self.reset()
-    
+        
     def did_change_size(self):
         pass
     
     def update(self):
         for c in self.children:
             if isinstance(c, Part):
-                c.update()
-                c.rotation = c.r
-                c.position = ((self.size.x / self.scale / 2) + c.x - self.ship.x, c.y)
+                c.update(self.dt)
+                if not self.mtdata.is_above_ground(c, 0):
+                    c.y = self.mtdata.get_y(c.x)
+                    c.vr = 0
+                    c.vx = -c.vx / 2
 
+                # viewport is centered on ship so adjust for that
+                c.position = ((self.size.x / self.scale / 2) + c.x - self.ship.x, c.y)
+                    
         if self.running:
             self.ship.update(self.dt)
 
@@ -309,31 +331,34 @@ class MyScene(Scene):
             if self.landed:
                 if self.mtdata.is_above_ground(self.ship, adj):
                     self.landed = False
-                    self.label.text = ''    
+                    self.label.text = ''
                 else:
                     self.ship.y = self.mtdata.get_y(self.ship.x) + adj
                     self.ship.null()
-            elif not self.mtdata.is_above_ground(self.ship, adj) and self.ship.vy < 0:
+            else:
                 level = self.mtdata.is_level(self.ship.x - 10, self.ship.x + 10)
-                self.label.text = 'vx:{:4.2f} vy:{:4.2f} l:{:}'.format(self.ship.vx, self.ship.vy, level)
-                if self.ship.vy < -1 or abs(self.ship.vx) > 0.30 or not level:
-                    self.crash()
-                else:
-                    self.land()
-                self.ship.y = self.mtdata.get_y(self.ship.x) + adj
+                hot = abs(self.ship.vy) > 1.0 or abs(self.ship.vx) > 0.30 or not level
 
-        self.set_scale()
+                self.label.text = f'vx:{self.ship.vx:+4.2f} vy:{self.ship.vy:+4.2f} r:{self.ship.r:4.2f} l:{level}'
+                self.label.color = 'red' if hot else 'white'
 
-        self.ship.rotation = self.ship.r
+                if not self.mtdata.is_above_ground(self.ship, adj) and self.ship.vy < 0:
+                    if hot:
+                        self.crash()
+                    else:
+                        self.land()
+                    self.ship.y = self.mtdata.get_y(self.ship.x) + adj
+
+        self.update_scale()
+
         self.ship.position = (self.size.x / self.scale / 2, self.ship.y)
         self.mt.position = ((self.size.x / self.scale / 2) - self.ship.x, 0)
 
         self.update_status()
 
     def update_status(self):
-        self.stats.text = 'f: {:+4.0f}  a: {:4.2f}  r: {:+5.3f} {:+5.3f}  v: {:+5.2f},{:+5.2f}  p: {:+4.1f},{:+4.0f},{:+4.0f} l: {:}'.format(
-            self.ship.fuel, self.ship.get_a() * 10, self.ship.ar * 1000, self.ship.vr * 1000, self.ship.vx,
-            self.ship.vy, self.ship.rotation, self.ship.x, self.ship.y, self.landings)
+        self.stats.text = 'f: {:+4.0f}  a: {:4.2f}  r: {:+5.3f} {:+5.3f}  p: {:+4.1f},{:+4.0f},{:+4.0f} l: {:}'.format(
+            self.ship.fuel, self.ship.a * 10, self.ship.ar * 1000, self.ship.vr * 1000, self.ship.rotation, self.ship.x, self.ship.y, self.landings)
 
     def touch_began(self, touch):
         if self.running:
@@ -344,11 +369,11 @@ class MyScene(Scene):
     def touch_moved(self, touch):
         if self.running:
             self.thrust_ramp = 0
-            self.ship.set_thrust(max((touch.location.y - self.touch_home.y - 5) / 10, 0))
-            if not self.landed and abs(touch.location.x - self.touch_home.x) > 15:
+            self.ship.set_thrust(max((-touch.location.y + self.touch_home.y - 5) / 10, 0))
+            if not self.landed and abs(touch.location.x - self.touch_home.x) > 20:
                 self.ship.rotate((touch.location.x - self.touch_home.x) / 50)
             else:
-                self.ship.rotate(0.0)           
+                self.ship.rotate(0.0)
 
     def touch_ended(self, touch):
         self.ship.thrust_ramp = 0.5
@@ -366,7 +391,7 @@ class MyScene(Scene):
         elif key == 'button_y' and value:
             self.ship.fuel += 100
 
-    def set_scale(self):
+    def update_scale(self):
         if self.ship.y > self.size.h * 0.75:
             value = self.size.h * 0.75 / abs(self.ship.y) + 0.01
         else:
@@ -381,12 +406,10 @@ class MyScene(Scene):
         self.label.scale = 1 / value
 
     def fire(self):
-        path = ui.Path()
+        path = make_path([(0, 0), (0, 1)])
         path.line_width = 2
         path.line_cap_style = ui.LINE_CAP_ROUND
         path.line_join_style = ui.LINE_JOIN_ROUND
-        path.move_to(0, 0)
-        path.line_to(0, 1)
         bullet = ShapeNode(path=path, fill_color='clear', stroke_color='white', parent=self)
 
         r = self.ship.rotation
@@ -399,18 +422,18 @@ class MyScene(Scene):
         sound.play_effect('fire.mp3', looping=False, volume=0.0)
 
     def drop_ship(self, length):
-        x = random.uniform(length/8, length * 7 / 8)
+        x = random.uniform(length / 8, length * 7 / 8)
 
         while x < length:
             if self.mtdata.is_level(x - 10, x + 10):
                 break
-            x += 10
+            x += 30
 
         self.ship.x = x
         self.ship.y = self.mtdata.get_y(x)
         self.landed = True
 
-        print('dropped:', self.ship.x, self.ship.y)
+        print('dropped:', self.ship.x, self.ship.y, self.ship.r)
 
     def crash(self):
         print('crash', self.ship.vx, self.ship.vy)
@@ -418,21 +441,14 @@ class MyScene(Scene):
         self.ship.crash()
         self.ship.remove_from_parent()
 
-        for x in [10, 10, 30, 30]:
-            p = Part(Point(x, 0))
-            p.x, p.y = self.ship.x, self.ship.y
-
-            p.vr = random.uniform(0.1, 0.3 )
-            p.vx = self.ship.vx + random.uniform(-1, 1)
-            p.vy = random.uniform(0, 3)
-            p.run_action(A.sequence(A.fade_to(0.50, 2), A.remove()))
-            self.add_child(p)
-
         self.running = False
 
     def land(self):
         self.landed = True
-        self.landings += 1
+        # only give credit for a real flight
+        if self.ship.maxalt - self.ship.y > 15:
+            self.ship.maxalt = self.ship.y
+            self.landings += 1
 
     def reset(self):
         self.ship.null()
@@ -440,6 +456,7 @@ class MyScene(Scene):
         self.add_child(self.ship)
         self.label.text = ''
         self.running = True
+        self.paused = False
     
 
 if __name__ == '__main__':
